a90b391f508708f3737f7211ee624be032c1ea35
error_99 <tylercuddy@gmail.com>
2016-04-23 00:33:23 -0700

    cleaned up css

--- script.js
+++ script.js
@@ -4,12 +4,12 @@
 
 $(document).ready(function () {
    //$('img').css('opacity','.5') ;
-   $('figure').fadeTo(0,'.5');
+   $('img').fadeTo(0,'.5');
    $('figure').mouseenter(function(){
-       $(this).fadeTo('fast','1');
+       $(this).find('img').fadeTo('fast','1');
    });
     $('figure').mouseleave(function () {
-       $(this).fadeTo('fast','.5') ;
+       $(this).find('img').fadeTo('fast','.5') ;
     });
 });
 
