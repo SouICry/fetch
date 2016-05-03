0b3673da0c4f0aec9ef7d7f73ec62ee93fe3dadc
error_99 <tylercuddy@gmail.com>
2016-05-02 18:03:42 -0700

    got some of the single page functionality working

--- frontEnd/frontPage/script.js
+++ frontEnd/frontPage/script.js
@@ -15,3 +15,97 @@ $(document).ready(function () {
     });
 });
 
+// write information about the state of the app in the url as #hash.
+// Hashes don’t cause the page to reload and are easily accessible and manipulated.
+$(function () {
+
+    // $('div').fadeIn('slow');
+    //render(decodeURI(window.location.hash));
+
+    function renderHomePage(){
+        // Shows the Single Product Page with appropriate data.
+        console.log("renderHomePage");
+        var page = $('#home');
+        console.log("page:",page);
+        page.removeClass('hidden');
+    }
+
+    function renderShopperPage(){
+        // Crates an object with filtered products and passes it to renderProductsPage.
+        var page = $('#shopper');
+        page.removeClass('hidden');
+    }
+
+    function renderDriverPage(){
+        // Shows the error page.
+        var page = $('#driver');
+        page.removeClass('hidden');
+    }
+
+    function render(url) {
+
+        console.log("render hash:",window.location.hash);
+        // Get the keyword from the url. in this case the keyword whatever comes before the
+        // the first slash so #one or #two
+        var temp = url.split('/')[0];
+        console.log("temp:",temp);
+
+        // Hide whatever page is currently shown.
+        $('.page').addClass('hidden');
+
+        // this is and instance of an object
+        var map = {
+
+            // The "Homepage".
+            '': function () {
+                renderHomePage();
+            },
+
+            // Single Products page.
+            '#home': function () {
+                console.log("map says hello");
+                renderHomePage();
+            },
+
+            // Page with filtered products
+            '#createList': function () {
+                renderShopperPage();
+            },
+            '#pickUpList': function () {
+                renderDriverPage();
+            }
+
+        };
+
+        // Execute the needed function depending on the url keyword (stored in temp).
+        if(map[temp]){
+            console.log("map[temp]:" ,map[temp]);
+            map[temp]();
+        }
+    };
+
+    // An event handler with calls the render function on every hashchange.
+    // The render function will show the appropriate content of out page.
+    $(window).on('hashchange', function(){
+        //decodeURI will just return the hash of the desired url ie #one
+        console.log("window.location.hash:",window.location.hash);
+        console.log("docdeURI:",decodeURI(window.location.hash));
+        render(decodeURI(window.location.hash));
+    });
+
+    $('.homePage').on('click', function(){
+        //console.log("window.location.hash:",window.location.hash);
+        window.location.hash='#home';
+    });
+
+    $('.createList').on('click', function(){
+        //console.log("window.location.hash:",window.location.hash);
+        window.location.hash='#createList';
+    });
+    $('.pickUpList').on('click', function(){
+        //console.log("window.location.hash:",window.location.hash);
+        window.location.hash='#pickUpList';
+    });
+});
+
+
