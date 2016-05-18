e9751779caaea7888efa2d9c8d9017a8d5871425
Guyue Zhou <souicry.email@gmail.com>
2016-05-18 11:52:45 -0700

    Added payment front end core code. Need integration

--- js/loader.js
+++ js/loader.js
@@ -2,15 +2,27 @@
 
 var loader = {
 
-    userId : "",
+    user: {
+        isLoggedIn: false,
+        id:""
+    },
+    ticket : {
+        id : "",
+        price: 5
+    },
+
 
     //Separate loading
     loadAbove: function(){
 
     },
     deleteAbove: function(){
-
+        if (this.onReturnBelow != null){
+            this.onReturnBelow();
+            this.onReturnBelow = null;
+        }
     },
+    onReturnBelow: null,
     loadedPages: ["homePage"],
     currentPage : "homePage",
     //One at a time
