3c7885b1b304ef280b6963dd10d814acbf910a01
Guyue Zhou <souicry.email@gmail.com>
2016-05-17 17:31:15 -0700

    Added wip payments

---
+++ js/loader.js
@@ -0,0 +1,34 @@
+
+
+var loader = {
+
+    userId : "",
+
+    //Separate loading
+    loadAbove: function(){
+
+    },
+    deleteAbove: function(){
+
+    },
+    loadedPages: ["homePage"],
+    currentPage : "homePage",
+    //One at a time
+    next : function(param){
+
+    },
+    previous : function(param){
+
+    },
+    //All pages
+    loadPages: function(){
+
+    },
+    deletePages: function(){
+
+    },
+
+
+
+};
+
