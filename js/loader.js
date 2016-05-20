f88f834af40f2269a2d63da8bfc4c2e5de3a5290
William Shin <w4shin@ucsd.edu>
2016-05-19 21:23:35 -0700

    undo loader

--- js/loader.js
+++ js/loader.js
@@ -1,1 +1,70 @@
-var loader = 999;
+var ticketState = {
+    draft: "draft",
+    queued: "queued",
+    selected: "selected",
+    approved: "approved",
+    shopping: "shopping",
+    checkout: "checkout",
+    complete: "complete"
+};
+
+var ticketOption = {
+    cheapest: "cheapest",
+    expensive: "expensive",
+    nothing: "nothing"
+};
+
+var loader = {
+    user: {
+        isLoggedIn: false,
+        isDriver: false,
+        id: "",
+        tickets: []
+    },
+    ticket: {
+        id: "",
+        state: ticketState.draft,
+        price: 5,
+        option: ticketOption.cheapest,
+        availableTimes: {}
+    },
+    loadAbove: function () {
+
+    },
+    deleteAbove: function () {
+        if (this.onReturnBelow != null) {
+            this.onReturnBelow();
+            this.onReturnBelow = null;
+        }
+    },
+    onReturnBelow: null,
+    loadedPages: {},
+    currentPage: "homePage",
+    //One at a time
+    next: function (param) {
+
+    },
+    previous: function (param) {
+
+    },
+    //All pages
+    loadPages: function () {
+
+    },
+    deletePages: function () {
+
+    },
+    init: function () {
+
+        $.ajax({
+            type: "GET",
+            url: "_signUp.html",
+            success: function (data) {
+                alert(data);
+                this.loadedPages.signUp = document.createElement("div");
+                $(this.loadedPages.signUp).html(data);
+            }
+        });
+
+    }
+};
