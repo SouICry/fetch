6bb847404b4188ac4a98d142d4192fadcf553c3e
Soheil <skarimik@ucsd.edu>
2016-05-21 16:04:35 -0700

    checkout updated

--- js/_checkout.js
+++ js/_checkout.js
@@ -1,59 +1,70 @@
-function _checkout() {
+(function() {
 
-    var _checkout = {
-        notes:"",
-        range1:"",
-        range2:""
+    loader._checkout = {
+        data: "", //Optional
+        version: 0, //Must be 0
+        getData: function () { //must be null if not needed
+
+            _checkout.notes = $('input[name="specialnotes"]:checked', '#notes').val();
+            _checkout.range1 = $("#time1").val();
+            _checkout.range2 = $("#time2").val();
+            return _checkout;
+        },
+        loadData: null,
     };
+        var _checkout = {
+            notes: "",
+            range1: "",
+            range2: ""
+        };
 
-    $('#submitcheckout').click(function () {
+        $('#submitcheckout').click(function () {
 
-        var valid = false;
+            var valid = false;
 
-        _checkout.notes = $('input[name="specialnotes"]:checked', '#notes').val();
-        _checkout.range1 = $("#time1").val();
-        _checkout.range2 = $("#time2").val();
+            _checkout.notes = $('input[name="specialnotes"]:checked', '#notes').val();
+            _checkout.range1 = $("#time1").val();
+            _checkout.range2 = $("#time2").val();
 
-        index1 = $("#time1").prop('selectedIndex');
+            index1 = $("#time1").prop('selectedIndex');
 
-        if ($("#time1").prop('selectedIndex') < $("#time2").prop('selectedIndex')){
-            valid = true;
-        }
-
-
-        if (valid == true) {
-            sendToServer();
-        }
-        else {
-            confirm("Enter a valid valid time range.");
-        }
-
-        loader.next();
-    });
-
-    function sendToServer(){
-        var info_to_send = {};
-        info_to_send.id = $('#user-name').data('id');
-        info_to_send.notesTime = _checkout;
-        info_to_send.type = "send";
-
-        //Simulation (alert or console.log to check for yourself)
-        alert(JSON.stringify(info_to_send));
-
-        //Actual
-        $.ajax({
-            type: "POST",
-            url: "/_checkout",
-            data: info_to_send,
-            success: function(data){
-                //data is the object sent back on success (could also just be string)
-                alert("Congrats!");
-            },
-            error: function(data){
-                //data is the object send back on fail (could also just be string)
+            if ($("#time1").prop('selectedIndex') < $("#time2").prop('selectedIndex')) {
+                valid = true;
             }
-        });
-    }
-}
 
-_checkout();
+
+            if (valid == true) {
+                sendToServer();
+            }
+            else {
+                confirm("Enter a valid valid time range.");
+            }
+
+            loader.next();
+        });
+
+        function sendToServer() {
+            var info_to_send = {};
+            info_to_send.id = $('#user-name').data('id');
+            info_to_send.notesTime = _checkout;
+            info_to_send.type = "send";
+
+            //Simulation (alert or console.log to check for yourself)
+            alert(JSON.stringify(info_to_send));
+
+            //Actual
+            $.ajax({
+                type: "POST",
+                url: "/_checkout",
+                data: info_to_send,
+                success: function (data) {
+                    //data is the object sent back on success (could also just be string)
+                    alert("Congrats!");
+                },
+                error: function (data) {
+                    //data is the object send back on fail (could also just be string)
+                }
+            });
+        }
+    
+})();
