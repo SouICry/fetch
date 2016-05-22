c6690e9a8c3541a38d4a0c758092e0a07f014afc
Soheil <skarimik@ucsd.edu>
2016-05-22 14:10:42 -0700

    homePage.js update

--- js/_homePage.js
+++ js/_homePage.js
@@ -1,17 +1,24 @@
 /** Created by tylercuddy on 5/4/16. */
+(function() {
+    loader._homePage = {
+        data: ["greenEggs", "ham"], //Optional
+        version: 0, //Must be 0
+        getData:null,
+        loadData: null,
+    };
+    $(document).ready(function () {
 
-$(document).ready(function () {
 
+        $('#homePage img').fadeTo(0, '.4');
+        $('.name').css('color', 'black');
+        $('figure').mouseenter(function () {
+            $(this).find('img').fadeTo('fast', '1');
+            $(this).find('.name').css('color', '#f36100');
+        });
+        $('#homePage figure').mouseleave(function () {
+            $(this).find('img').fadeTo('fast', '.4');
+            $(this).find('.name').css('color', '#000000');
 
-    $('#homePage img').fadeTo(0,'.4');
-    $('.name').css('color','black');
-    $('figure').mouseenter(function(){
-        $(this).find('img').fadeTo('fast','1');
-        $(this).find('.name').css('color','#f36100');
+        });
     });
-    $('#homePage figure').mouseleave(function () {
-        $(this).find('img').fadeTo('fast','.4') ;
-        $(this).find('.name').css('color','#000000');
-
-    });
-});
+})();
