e1d4a6014f597fa12a251113cb2863eaa189fd3f
itsjen <jeanettephung@hotmail.com>
2016-05-22 21:38:07 -0700

    shopping data

--- js/_shopping.js
+++ js/_shopping.js
@@ -1,40 +1,57 @@
 (function () {
     loader._shopping = {
+        //data: "none",
         data: ["greenEggs", "ham"], //Optional
         version: 0, //Must be 0
         getData: function () { //must be null if not needed
             return list_shopping;
         },
-        loadData: function (data) { 
-            var arr = [];
-
+        loadData: function (data) {
+            if (data == "none" || data.length == 0) {
+                $("#submit_list").html("");
+            }
+            
             $("#submit_list").html("");
             shopping_count = 0;
-            list_shopping.splice(0,list_shopping.length);
+            list_shopping.splice(0, list_shopping.length);
+            $("#list").empty();
 
             for (var i = 0; i < data.length; i++) {
                 shoppping_toAdd = data[i];
                 var newItem = document.createElement('li');
                 newItem.innerHTML = shoppping_toAdd;
                 newItem.className = 'item';
-                $("list").append(newItem);
+                $("#list").append(newItem);
 
                 shopping_count++;
                 if (shopping_count == 1) {
                     $("#numItems").text("1 item");
                 }
                 else {
-                    $("#numItems").text(shopping_count + " data");
+                    $("#numItems").text(shopping_count + " items");
                 }
+
                 if (shopping_count != 0) {
-                    $("#footerInfo, .footerBars").show();
+                    $("#footerInfo, .footerBars, #submit_list").show();
                 }
                 else {
-                    $("#footerInfo,.footerBars").hide();
+                    $("#footerInfo, .footerBars").hide();
                 }
             }
-        }
+            if (shopping_count == 1) {
+                $("#numItems").text("1 item");
+            }
+            else {
+                $("#numItems").text(shopping_count + " items");
+            }
 
+            if (shopping_count != 0) {
+                $("#footerInfo, .footerBars, #submit_list").show();
+            }
+            else {
+                $("#footerInfo, .footerBars").hide();
+            }
+        }
     };
 
     var shopping_count = 0;
@@ -49,11 +66,8 @@
         }
     });
 
-
     $('#shopping_list_form').submit(addItem);
     function addItem() {
-        event.preventDefault();
-
         if ($('#shoppingCheckListItem').val() !== '') {
             shoppping_toAdd = $('#shoppingCheckListItem').val();
             var newItem = document.createElement('li');
@@ -76,14 +90,20 @@
                 $("#footerInfo, .footerBars").hide();
             }
 
+            event.preventDefault();
+
+            $('#shoppingCheckListItem').val('');
+
             loader.currentPageChanged();
+
+            list_shopping.push(shoppping_toAdd);
+
         }
-        $('#shoppingCheckListItem').val('');
-
-        list_shopping.push(shoppping_toAdd);
+        else {
+            event.preventDefault();
+        }
     }
-
-
+    
     // remove item
     $(document).on('click', '.item', function () {
         $(this).remove();
@@ -104,7 +124,6 @@
             $("#submit_list, #footerInfo, .footerBars").show();
         }
 
-
         var index = list_shopping.indexOf($(this).text());
 
         if (index > -1) {
@@ -113,9 +132,6 @@
 
         loader.currentPageChanged();
     });
-
-
-
 })();
 
 
