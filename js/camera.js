17578b5de2c602695acb79800dce5aee0507d6ad
Guyue Zhou <souicry.email@gmail.com>
2016-05-23 14:07:55 -0700

    cmera test

--- js/camera.js
+++ js/camera.js
@@ -1,7 +1,7 @@
 /*
-https://davidwalsh.name/browser-camera
-https://stackoverflow.com/questions/18483160/which-camera-will-open-getusermedia-api-in-mobile-device-front-or-rear
-https://codeforgeek.com/2014/11/file-uploads-using-node-js/
+ https://davidwalsh.name/browser-camera
+ https://stackoverflow.com/questions/18483160/which-camera-will-open-getusermedia-api-in-mobile-device-front-or-rear
+ https://codeforgeek.com/2014/11/file-uploads-using-node-js/
 
 
 
