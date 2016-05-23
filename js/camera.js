ae0e543c76e299b8b937416e2fe5e9d36cb6ac04
Guyue Zhou <souicry.email@gmail.com>
2016-05-23 16:33:59 -0700

    Added camera script

--- js/camera.js
+++ js/camera.js
@@ -7,3 +7,83 @@
 
 
  */
+$(document).ready(function () {
+    'use strict';
+    var videoElement = null;
+    var audioSelect = document.getElementById('audioSource');
+    var videoSelect = document.getElementById('videoSource');
+
+    navigator.getUserMedia = navigator.getUserMedia ||
+        navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+    function gotSources(sourceInfos) {
+        for (var i = 0; i !== sourceInfos.length; ++i) {
+            var sourceInfo = sourceInfos[i];
+            var option = document.createElement('option');
+            option.value = sourceInfo.id;
+            if (sourceInfo.kind === 'audio') {
+                option.text = sourceInfo.label || 'microphone ' +
+                    (audioSelect.length + 1);
+                audioSelect.appendChild(option);
+            } else if (sourceInfo.kind === 'video') {
+                option.text = sourceInfo.label || 'camera ' + (videoSelect.length + 1);
+                videoSelect.appendChild(option);
+            } else {
+                console.log('Some other kind of source: ', sourceInfo);
+            }
+        }
+    }
+
+    if (typeof MediaStreamTrack === 'undefined' ||
+        typeof MediaStreamTrack.getSources === 'undefined') {
+        alert('Your browser doesnt support using the camera :( Try uploading or use Chrome instead');
+    } else {
+        MediaStreamTrack.getSources(gotSources);
+    }
+
+    function successCallback(stream) {
+        window.stream = stream; // make stream available to console
+        videoElement.src = window.URL.createObjectURL(stream);
+        videoElement.play();
+    }
+
+    function errorCallback(error) {
+        console.log('navigator.getUserMedia error: ', error);
+        alert('Something went wrong with the camera :( Try uploading or use Chrome instead');
+    }
+
+    function enableCamera(vid, source) {
+        if (arguments.length > 0) {
+            videoElement = vid;
+        }
+        if (arguments.length > 1) {
+            source = 0;
+        }
+        videoSelect.selectedIndex = source;
+        var audioSource = audioSelect.value;
+        var videoSource = videoSelect.value;
+        var constraints = {
+            audio: {
+                optional: [{
+                    sourceId: audioSource
+                }]
+            },
+            video: {
+                optional: [{
+                    sourceId: videoSource
+                }]
+            }
+        };
+        navigator.getUserMedia(constraints, successCallback, errorCallback);
+    }
+
+    function disableCamera() {
+        if (window.stream) {
+            videoElement.src = null;
+            window.stream.getTracks().forEach(function (track) {
+                track.stop();
+            });
+            window.stream = null;
+        }
+    }
+});
