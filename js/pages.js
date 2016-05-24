ba8746c02142ff8cc7af1262e94e88f0d8b2fafd
itsjen <jeanettephung@hotmail.com>
2016-05-24 00:53:41 -0700

    shoppingStatus basics

--- js/pages.js
+++ js/pages.js
@@ -6,10 +6,9 @@ var allPages = [
     ["_accSetting", "_contact", "_passwordRecovery", "_passwordReset", "_login", "_signUp" /*", _takeProfilePicture"*/],
     
     ["_homePage", "_shopping", "_checkout"/*, "_pendingPayment", "_submitted"*/],
-    ["_history"/*, "_shoppingStatus"*/],
+    ["_history", "_shoppingStatus"],
     [/*'_confirmTicket',*/ "_rateDriver" /*,'_congratsTicketClosed'*/],
     
     ['_tickets' /*, '_viewTicket'*/],
     ["_yourDeliveries", '_driverList', /*_receiptPictureEnterPrice,*/"_congrats_driver_finish_shopping"],
-    [/*'_confirmCompletion', '_rateUser',*/ '_congrats']
 ];
