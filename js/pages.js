acad5b929260c36be6cded623b90deed4614c534
Guyue Zhou <souicry.email@gmail.com>
2016-05-24 01:49:51 -0700

    Fixed pages.js

--- js/pages.js
+++ js/pages.js
@@ -4,16 +4,12 @@ var frontPage = [
 
 var allPages = [
     ["_accSetting", "_contact", "_passwordRecovery", "_passwordReset", "_login", "_signUp" /*", _takeProfilePicture"*/],
-    
+
     ["_homePage", "_shopping", "_checkout"/*, "_pendingPayment", "_submitted"*/],
-<<<<<<< HEAD
     ["_history"/*, "_shoppingStatus"*/],
-    ["_confirmTicket", "_rateDriver" ,'_congrats_ticket_closed'],
-=======
-    ["_history", "_shoppingStatus"],
-    [/*'_confirmTicket',*/ "_rateDriver" /*,'_congratsTicketClosed'*/],
->>>>>>> 7b9c4bdcc57fd08bee266cee2d69f954fd4a74ca
-    
+    ['_confirmTicket', "_rateDriver" ,'_congratsTicketClosed'],
+
     ['_tickets' , '_viewTicket'],
     ["_yourDeliveries", '_driverList', /*_receiptPictureEnterPrice,*/"_congrats_driver_finish_shopping"],
+    [/*'_confirmCompletion', '_rateUser',*/ '_congrats']
 ];
