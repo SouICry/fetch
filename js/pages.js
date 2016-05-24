1fb75048d3c269ddb03a30a7b52a02e1cbc2534a
Guyue Zhou <souicry.email@gmail.com>
2016-05-24 01:51:34 -0700

    Slight pages.js change

--- js/pages.js
+++ js/pages.js
@@ -1,12 +1,8 @@
-var frontPage = [
-    //On desktop, shift to right side and fill empty space with about info or image
-];
-
 var allPages = [
     ["_accSetting", "_contact", "_passwordRecovery", "_passwordReset", "_login", "_signUp" /*", _takeProfilePicture"*/],
 
     ["_homePage", "_shopping", "_checkout"/*, "_pendingPayment", "_submitted"*/],
-    ["_history"/*, "_shoppingStatus"*/],
+    ["_history"/*, "_shoppingStatus", "_cancelTicket"*/],
     ['_confirmTicket', "_rateDriver" ,'_congratsTicketClosed'],
 
     ['_tickets' , '_viewTicket'],
