9f490896d67dc3588737398d19b4f42b4200fe26
itsjen <jeanettephung@hotmail.com>
2016-05-25 00:45:17 -0700

    accsetting js to get/send data

--- js/_accSetting.js
+++ js/_accSetting.js
@@ -1,60 +1,81 @@
 (function () {
     loader._accSetting = {
-        data: "", //Optional
-        version: 0, //Must be 0
-        getData: null,  //must be null if not needed
-        loadData: null,  // MUST RESET PAGE AS WELL
-
+        data: {
+            full_name: "Jeanette Phung", email: "jeanettephung@hotmail.com", phone: "(626)443-5688",
+            street: "Gage Ave.", state: "CA", city: "El Monte", zip: 91731
+        },
+        version: 0, //Must be 0 
+        getData: function () {
+            return _account;
+        },
+        loadData: function (data) {
+            $("#accsetting_full_name").val(data.full_name);
+            $("#accsetting_email").val(data.email);
+            $("#accsetting_phone").val(data.phone);
+            $("#accsetting_street").val(data.street);
+            $("#accsetting_city").val(data.city);
+            $("#accsetting_state").val(data.state);
+            $("#accsetting_zip").val(data.zip);
+        }
     };
-    $('#submit_info').prop('disabled', true);
-    $('#accsetting_email,#accsetting_full_name,#accsetting_phone,#accsetting_street,#accsetting_city,#accsetting_state,#accsetting_zip').keyup(function () {
-        if ($('#accsetting_email').val() && $('#accsetting_full_name').val() && $('#accsetting_phone').val()&& $('#accsetting_street').val() && $('#accsetting_city').val() && $('#accsetting_state').val() && $('#accsetting_zip').val()) {
-            $('#submit_info').prop('disabled', false);
-        }
-        else {
-            $('#submit_info').prop('disabled', true);
-        }
-    });
 
+    var _account = {};
 
-    $('#submit_info').click(function () {
-        asshole3();
-    });
-
-    function asshole3() {
-        var _account = {
+    $("#accSetting_submit_info").click(function () {
+        _account = {
             full_name: $('#accsetting_full_name').val(),
             email: $('#accsetting_email').val(),
             phone: $('#accsetting_phone').val(),
-            address: {
-                street: $('#accsetting_street_').val(),
-                city: $('#accsetting_city').val(),
-                state: $('#accsetting_state').val(),
-                zip: $('#accsetting_zip').val()
-            }
-        }
-        var info_to_send = {};
-        info_to_send.user = _account;
-        info_to_send.type = "send";
+            street: $('#accsetting_street_').val(),
+            city: $('#accsetting_city').val(),
+            state: $('#accsetting_state').val(),
+            zip: $('#accsetting_zip').val()
+        };
+    });
+})();
 
-        //Simulation (alert or console.log to check for yourself)
-        alert(JSON.stringify(info_to_send));
 
-        //Actual
-        $.ajax({
-            type: "POST",
-            url: "/_accSetting", //TODO is this should be accountSetting post,
-            data: info_to_send,
-            success: function (data) {
-                //data is the object sent back on success (could also just be string)
-                alert("Congrats!");
+// var _account = {};
+//
+// $('#accsetting_submit_info').click(function () {
+//     asshole3();
+// });
 
-            },
-            error: function (data) {
-                //data is the object send back on fail (could also just be string)
-            }
-        });
-    }
-});
+
+// function asshole3() {
+//     _account = {
+//         full_name: $('#accsetting_full_name').val(),
+//         email: $('#accsetting_email').val(),
+//         phone: $('#accsetting_phone').val(),
+//         address: {
+//             street: $('#accsetting_street_').val(),
+//             city: $('#accsetting_city').val(),
+//             state: $('#accsetting_state').val(),
+//             zip: $('#accsetting_zip').val()
+//         }
+//     }
+//     var info_to_send = {};
+//     info_to_send.user = _account;
+//     info_to_send.type = "send";
+//
+//     //Simulation (alert or console.log to check for yourself)
+//     alert(JSON.stringify(info_to_send));
+//
+//     //Actual
+//     $.ajax({
+//         type: "POST",
+//         url: "/_accSetting", //TODO is this should be accountSetting post,
+//         data: info_to_send,
+//         success: function (data) {
+//             //data is the object sent back on success (could also just be string)
+//             alert("Congrats!");
+//
+//         },
+//         error: function (data) {
+//             //data is the object send back on fail (could also just be string)
+//         }
+//     });
+// }
+
 
 
