5b12f2edf559a8321aafda63a3f557141c77196a
Soheil <skarimik@ucsd.edu>
2016-05-24 18:13:49 -0700

    updates

--- js/_index.js
+++ js/_index.js
@@ -63,7 +63,11 @@ function whenLogedIn(){
 $(document).ready(function (){  
     $("#Butt").append("<div class='panel-divs'id='index-Login'onclick=goToPage('_login'); > <span class= 'glyphicon glyphicon-log-in'></span> Login</div>");
     $("#Butt").append("<div class='panel-divs'id='index-SignUp'onclick=goToPage('_signUp'); > <span class= 'glyphicon glyphicon-plus-sign'></span> Sign Up </div>");
-    
+    $("#Butt").append("<div id='index-Tickets' onclick=goToPage('_tickets'); > <span class= ''></span> Tickets </div>");
+    $("#Butt").append("<div id='index-DeliverList' onclick=goToPage('_driverList2'); > <span class= ''></span> Dirver List </div>");
+    $("#Butt").append("<div id='index-CheckOut' onclick=goToPage('_checkout'); > <span class= 'glyphicon glyphicon-check'></span> Checkout </div>");
+    $("#Butt").append("<div id='index-Contact' onclick=goToPage('_contact'); > <span class= 'glyphicon glyphicon-equalizer'></span> Contact </div>");
+    $("#Butt").append("<div id='index-Shopping' onclick=goToPage('_shopping'); > <span class= ''></span> Shopping </div>");
 });
 
 function toggleShopper(){
@@ -73,9 +77,10 @@ function toggleShopper(){
     $("#index-Rating").remove();
     $("#index-LogOut").remove();
     $("#Butt").append("<div class='panel-divs' id='index-Home' onclick=goToPage('_homePage'); > <span class= 'glyphicon glyphicon-home'></span> Home </div>");
-    $("#Butt").append("<div id='index-History' onclick=goToPage('_history'); > <span class= ''></span> History </div>");
-    $("#Butt").append("<div id='index-Setting' onclick=goToPage('_accSetting'); > <span class= 'glyphicon glyphicon-cog'></span> Setting</div>");
-    $("#Butt").append("<div id='index-LogOut' onclick=goToPage('_homePage'); > <span class= 'glyphicon glyphicon-off'></span> Log Out </div>");
+    $("#Butt").append("<div class='panel-divs'id='index-History' onclick=goToPage('_history'); > <span class= ''></span> History </div>");
+    $("#Butt").append("<div class='panel-divs'id='index-Setting' onclick=goToPage('_accSetting'); > <span class= 'glyphicon glyphicon-cog'></span> Setting</div>");
+    $("#Butt").append("<div class='panel-divs'id='index-LogOut' onclick=goToPage('_homePage'); > <span class= 'glyphicon glyphicon-off'></span> Log Out </div>");
+    
 
 
 
@@ -86,10 +91,10 @@ function toggleDriver(){
     $("#index-Setting").remove();
     $("#index-LogOut").remove();
     $("#Butt").append("<div class='panel-divs' id='index-Home' onclick=goToPage('_homePage'); > <span class= 'glyphicon glyphicon-home'></span> Home </div>");
-    $("#Butt").append("<div id='index-Deliveries' onclick=goToPage('_yourDeliveries'); > <span class= ''></span> Your Deliveries </div>");
-    $("#Butt").append("<div id='index-Setting' onclick=goToPage('_accSetting'); > <span class= 'glyphicon glyphicon-cog'></span> Setting</div>");
-    $("#Butt").append("<div id='index-Rating' onclick=goToPage('_rating'); > <span class= 'glyphicon glyphicon-signal'></span> Rating </div>");
-    $("#Butt").append("<div id='index-LogOut' onclick=goToPage('_homePage'); > <span class= 'glyphicon glyphicon-off'></span> Log Out </div>");
+    $("#Butt").append("<div class='panel-divs'id='index-Deliveries' onclick=goToPage('_yourDeliveries'); > <span class= ''></span> Your Deliveries </div>");
+    $("#Butt").append("<div class='panel-divs'id='index-Setting' onclick=goToPage('_accSetting'); > <span class= 'glyphicon glyphicon-cog'></span> Setting</div>");
+    $("#Butt").append("<div class='panel-divs'id='index-Rating' onclick=goToPage('_rating'); > <span class= 'glyphicon glyphicon-signal'></span> Rating </div>");
+    $("#Butt").append("<div class='panel-divs'id='index-LogOut' onclick=goToPage('_homePage'); > <span class= 'glyphicon glyphicon-off'></span> Log Out </div>");
 
 }
 function toggles(){
