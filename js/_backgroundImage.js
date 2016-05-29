cda7b80abaa133161593f0b7dcf275579c420dd9
error_99 <tylercuddy@gmail.com>
2016-05-28 21:20:38 -0700

    changed backgroundImage

--- js/_backgroundImage.js
+++ js/_backgroundImage.js
@@ -4,11 +4,11 @@
 
 (function () {
     var images = [], x = 0;
-    images[0] = "/resources/images/beet-on-pink.jpg";
-    images[1] = "/resources/images/lemon-on-blue.jpg";
-    images[2] = "/resources/images/carrot-on-pink.jpg";
-    images[3] = "/resources/images/peppers.jpg";
-    images[4] = "/resources/images/cucumber-on-green.jpg";
+    images[0] = "/resources/images/lemon-on-blue.jpg";
+    images[1] = "/resources/images/carrot-on-pink.jpg";
+    images[2] = "/resources/images/peppers.jpg";
+    images[3] = "/resources/images/cucumber-on-green.jpg";
+    images[4] = "/resources/images/beet-on-pink.jpg";
 
     function run(interval) {
 
