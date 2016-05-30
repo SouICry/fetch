b38ed372b84364ff68872ae1b1c8908005ae1795
Soheil <skarimik@ucsd.edu>
2016-05-30 14:52:06 -0700

    user name

--- js/_login.js
+++ js/_login.js
@@ -38,8 +38,10 @@
                 success: function (data) {
                     //data is the object sent back on success (could also just be string)
                     index_login();
+                    loadAccountData();
                     loader.login(data);
-                    document.getElementById("index_user-name").innerHTML = data.full_name;
+                    
+                    
                     
                     //alert('data after login: ' + data.full_name);
                 },
