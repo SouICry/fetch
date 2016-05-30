9eab38e3f403a2240bb902f10342dcacc15b9f0a
timothypei <tpei@ucsd.edu>
2016-05-30 15:17:54 -0700

    added the driver map stuff

--- js/driverMap.js
+++ js/driverMap.js
@@ -1,35 +1,75 @@
-function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 8,
-        center: {lat: 40.731, lng: -73.997}
-    });
-    var geocoder = new google.maps.Geocoder;
-    var infowindow = new google.maps.InfoWindow;
+var directionsDisplay;
+var directionsService;
+var map;
+var destGeo;
 
-    document.getElementById('submit').addEventListener('click', function() {
-        geocodeLatLng(geocoder, map, infowindow);
-    });
+(function () {
+    loader._accSetting = {
+        data: {
+            full_name: "Jeanette Phung", email: "jeanettephung@hotmail.com", phone: "(626)443-5688",
+            street: "Gage Ave.", state: "CA", city: "El Monte", zip: 91731
+        },
+        version: 0, //Must be 0 
+        getData: null,
+        loadData: null,
+        onPageLoad: function () {
+            getGeolocation
+        }
+    }
+})();
+
+function initialize() {
+    directionsService = new google.maps.DirectionsService();
+    directionsDisplay = new google.maps.DirectionsRenderer();
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            var mapOptions = {
+                zoom: 16,
+                center: {lat: pos.lat, lng: pos.lng}
+            };
+            map = new google.maps.Map(document.getElementById("driverMap"), mapOptions);
+            directionsDisplay.setMap(map);
+            
+            setTimeout(calcRoute, 500);
+        });
+    }
 }
 
-function geocodeLatLng(geocoder, map, infowindow) {
-    var input = document.getElementById('latlng').value;
-    var latlngStr = input.split(',', 2);
-    var latlng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
-    geocoder.geocode({'location': latlng}, function(results, status) {
-        if (status === google.maps.GeocoderStatus.OK) {
-            if (results[1]) {
-                map.setZoom(11);
-                var marker = new google.maps.Marker({
-                    position: latlng,
-                    map: map
-                });
-                infowindow.setContent(results[1].formatted_address);
-                infowindow.open(map, marker);
-            } else {
-                window.alert('No results found');
-            }
-        } else {
-            window.alert('Geocoder failed due to: ' + status);
+function calcRoute() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var start = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            //var end = new google.maps.LatLng(data.lat, data.lng);
+            var end = new google.maps.LatLng(destGeo.lat,destGeo.lng);
+            var request = {
+                origin: start,
+                destination: end,
+                travelMode: google.maps.TravelMode.DRIVING
+            };
+            directionsService.route(request, function (result, status) {
+                if (status == google.maps.DirectionsStatus.OK) {
+                    directionsDisplay.setDirections(result);
+                }
+            });
+        });
+    }
+}
+
+function getGeolocation() {
+    $.ajax({
+        type: "POST",
+        url: "/_driverMap",
+        data: null,
+        success: function (data) {
+            destGeo = data;
+        },
+        error: function (data) {
+            //data is the object send back on fail (could also just be string)
         }
     });
 }
+
