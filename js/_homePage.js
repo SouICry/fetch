f3aa67f0ee00939a5006c03157662fa965adfaa3
itsjen <jeanettephung@hotmail.com>
2016-05-31 15:41:48 -0700

    revert homePage

--- js/_homePage.js
+++ js/_homePage.js
@@ -20,9 +20,6 @@
     });
     $('#homePage figure').click(function(){
         loader.store = $(this).data("store");
-        loader._checkout.loadData(null);
-        loader._deliveryTime.loadData(null);
-        loader._shopping.loadData(null);
         goToPage("_shopping");
     });
 })();
