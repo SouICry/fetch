54af0239e3f02d4b9d7c55a948f36dd569c7c518
William Shin <w4shin@ucsd.edu>
2016-05-30 23:26:11 -0700

    FIXED SESSIOFN FREAKIN JEANETTE

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(session({resave: true, saveUninitialized: true, secret: 'williamiscool'}));
 app.use(passport.initialize());
-app.use(passport.sessiofn());
+app.use(passport.session());
 app.use(express.static(__dirname));
 
 var masters = {};
