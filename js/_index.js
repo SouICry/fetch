360d7f21dac83a5d92a5cfa6615aa111ba806c81
Soheil <skarimik@ucsd.edu>
2016-05-31 01:57:46 -0700

    update

--- js/_index.js
+++ js/_index.js
@@ -1,68 +1,7 @@
 /**
  * Created by zeusn_000 on 5/21/2016.
  */
-(function () {
 
-    var PanelLogin
-    var all = ["_accSetting", "_contact", "_history", "_passwordRecovery", "_homePage", "_shopping", "_checkout", "_login",
-        "_rating", "_passwordReset", "_signUp", "_tickets", "_driverList2", '_congrats', "_yourDeliveries"];
-
-    var allGlyphs = ["glyphicon glyphicon-cog", "", "", "glyphicon glyphicon-wrench", "glyphicon glyphicon-home",
-        "", "glyphicon glyphicon-check", "glyphicon glyphicon-log-in", "glyphicon glyphicon-signal",
-        "glyphicon glyphicon-repeat",
-        "glyphicon glyphicon-plus-sign", "", "", ""];
-
-    var pageNames = ["Setting", "Contact", "History", "Password Recovery", "Home", "Shopping", "Checkout", "Login",
-        "Rating", "Password Reset", "Sign Up", "Tickets", "Driver List", 'Congrats', "Your Deliveries"];
-
-
-
-
-
-    // $(document).ready(function () {
-    //     var i;
-    //     var whichid;
-    //     var glyph;
-    //     var name;
-    //     for (var j = 0; j < allSections.length; j++) {
-    //         var sectionName = allSections[j];
-    //         var section = allPages[sectionName];
-    //         for (i = 0; i < section.length; i++) {
-    //             whichid = section[i];
-    //             for (var k = 0; k < all.length; k++) {
-    //                 if (whichid == all[k]) {
-    //                     glyph = allGlyphs[k];
-    //                     name = pageNames[k];
-    //                     break;
-    //                 }
-    //             }
-    //             var clickVal = "onclick=\"goToPage('" + whichid + "');\"";
-    //             $("#Butt").append("<div " + clickVal + " > <span class= '" + glyph + "'></span> " + name + "   </div>");
-    //         }
-    //     }
-    // });
-    $(document).on('click', '.toggle-button', function() {
-        $(this).toggleClass('toggle-button-selected');
-    });
-})();
-var toggle = "Shopper";
-function whenLogedIn(){
-
-    var clickVal = "onclick=\"goToPage('" + whichid + "');\"";
-    $("#index-Login").remove();
-    $("#index-SignUp").remove();
-    $("#Butt").append("<div class='panel-divs' id='index-Home' onclick=goToPage('_homePage'); > <span class= 'glyphicon glyphicon-home'></span> Home </div>");
-    $("#Butt").append("<div class='panel-divs'id='index-Setting' onclick=goToPage('_accSetting'); > <span class= 'glyphicon glyphicon-cog'></span> Setting</div>");
-    $("#Butt").append("<div class='panel-divs'id='index-Contact' onclick=goToPage('_contact'); > <span class= 'glyphicon glyphicon-equalizer'></span> Contact </div>");
-    $("#Butt").append("<div class='panel-divs'id='index-History' onclick=goToPage('_history'); > <span class= ''></span> History </div>");
-    $("#Butt").append("<div class='panel-divs'id='index-Shopping' onclick=goToPage('_shopping'); > <span class= ''></span> Shopping </div>");
-    $("#Butt").append("<div class='panel-divs'id='index-CheckOut' onclick=goToPage('_checkout'); > <span class= 'glyphicon glyphicon-check'></span> Checkout </div>");
-    $("#Butt").append("<div class='panel-divs'id='index-Rating' onclick=goToPage('_rating'); > <span class= 'glyphicon glyphicon-signal'></span> Rating </div>");
-    $("#Butt").append("<div class='panel-divs'id='index-Tickets' onclick=goToPage('_tickets'); > <span class= ''></span> Tickets </div>");
-    $("#Butt").append("<div class='panel-divs'id='index-DeliverList' onclick=goToPage('_driverList'); > <span class= ''></span> Dirver List </div>");
-    $("#Butt").append("<div class='panel-divs'id='index-Deliveries' onclick=goToPage('_yourDeliveries'); > <span class= ''></span> Your Deliveries </div>");
-    $("#Butt").append("<div class='panel-divs'id='index-LogOut' onclick=loader.logout(); > <span class= 'glyphicon glyphicon-off'></span> Log Out </div>");
-}
 (function () {
     // Wait for everything to load, then check loader.isLoggedIn
     setTimeout(function() {
