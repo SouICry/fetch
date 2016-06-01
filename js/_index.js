419447dc1f083c9679661c22b85de5e97545a260
ChubbyBunny <trivi7300@gmail.com>
2016-06-01 01:25:00 -0700

    fix toggle

--- js/_index.js
+++ js/_index.js
@@ -20,7 +20,6 @@
 //     $("#Butt").append("<div class='panel-divs'id='index-Deliveries' onclick=goToPage('_yourDeliveries'); > <span class= ''></span> Your Deliveries </div>");
 //     $("#Butt").append("<div class='panel-divs'id='index-LogOut' onclick=loader.logout(); > <span class= 'glyphicon glyphicon-off'></span> Log Out </div>");
 // }
-(function () {
     var toggle = "Shopper";
     // Wait for everything to load, then check loader.isLoggedIn
     setTimeout(function() {
@@ -56,78 +55,79 @@
             $("#Butt").append("<div class='panel-divs'id='index-SignUp'onclick=goToPage('_signUp'); > <span class= 'glyphicon glyphicon-plus-sign'></span> Sign Up </div>");
         }
     }, 100);
-})();
-$("#open-menu-drawer").on('click',function(event){
-    if(loader.isLoggedIn) {
+
+
+    $("#open-menu-drawer").on('click',function(event){
+        if(loader.isLoggedIn) {
+            $("#User-Info").show();
+            $("#toggleButton").show();
+            $("#index-Login").remove();
+            $("#index-SignUp").remove();
+            loadAccountData();
+            changePanel();
+        }
+
+    });
+    function changePanel(){
+
+    }
+    function index_login(){
+        $("#open-menu-drawer").show();
+        $("#index_login_top").hide();
         $("#User-Info").show();
-        $("#toggleButton").show();
         $("#index-Login").remove();
         $("#index-SignUp").remove();
-        loadAccountData();
-       changePanel();
+        $("#index-Home").remove();
+        $("#index-Contact").remove();
+        $("#index-Setting").remove();
+        $("#index-LogOut").remove();
+        $("#Butt").append("<div class='panel-divs' id='index-Home' onclick=goToPage('_homePage'); > <span class= 'glyphicon glyphicon-home'></span> Home </div>");
+        $("#Butt").append("<div class='panel-divs'id='index-Contact' onclick=goToPage('_contact'); > <span class= 'glyphicon glyphicon-equalizer'></span> Contact </div>");
+        $("#Butt").append("<div class='panel-divs'id='index-Setting' onclick=goToPage('_accSetting'); > <span class= 'glyphicon glyphicon-cog'></span> Setting</div>");
+
+        $("#Butt").append("<div class='panel-divs'id='index-History' onclick=goToPage('_history'); > <span class= 'glyphicon glyphicon-shopping-cart'></span> Your Orders </div>");
+        $("#Butt").append("<div class='panel-divs'id='index-LogOut' onclick=loader.logout(); > <span class= 'glyphicon glyphicon-off'></span> Log Out </div>");
+
     }
 
-});
-function changePanel(){
-    
-}
-function index_login(){
-    $("#open-menu-drawer").show();
-    $("#index_login_top").hide();
-    $("#User-Info").show();
-    $("#index-Login").remove();
-    $("#index-SignUp").remove();
-    $("#index-Home").remove();
-    $("#index-Contact").remove();
-    $("#index-Setting").remove();
-    $("#index-LogOut").remove();
-    $("#Butt").append("<div class='panel-divs' id='index-Home' onclick=goToPage('_homePage'); > <span class= 'glyphicon glyphicon-home'></span> Home </div>");
-    $("#Butt").append("<div class='panel-divs'id='index-Contact' onclick=goToPage('_contact'); > <span class= 'glyphicon glyphicon-equalizer'></span> Contact </div>");
-    $("#Butt").append("<div class='panel-divs'id='index-Setting' onclick=goToPage('_accSetting'); > <span class= 'glyphicon glyphicon-cog'></span> Setting</div>");
-
-    $("#Butt").append("<div class='panel-divs'id='index-History' onclick=goToPage('_history'); > <span class= 'glyphicon glyphicon-shopping-cart'></span> Your Orders </div>");
-    $("#Butt").append("<div class='panel-divs'id='index-LogOut' onclick=loader.logout(); > <span class= 'glyphicon glyphicon-off'></span> Log Out </div>");
-    
-}
-
-function toggleShopper(){
-    $("#index-Deliveries").remove();
-    $("#index-Tickets").remove();
-    $("#index-History").remove();
-    $("#index-LogOut").remove();
-    $("#Butt").append("<div class='panel-divs'id='index-History' onclick=goToPage('_history'); > <span class= 'glyphicon glyphicon-shopping-cart'></span> Your Orders </div>");
-    $("#Butt").append("<div class='panel-divs'id='index-LogOut' onclick=loader.logout(); > <span class= 'glyphicon glyphicon-off'></span> Log Out </div>");
-    loader.switchToShopper();
-
-  
+    function toggleShopper(){
+        $("#index-Deliveries").remove();
+        $("#index-Tickets").remove();
+        $("#index-History").remove();
+        $("#index-LogOut").remove();
+        $("#Butt").append("<div class='panel-divs'id='index-History' onclick=goToPage('_history'); > <span class= 'glyphicon glyphicon-shopping-cart'></span> Your Orders </div>");
+        $("#Butt").append("<div class='panel-divs'id='index-LogOut' onclick=loader.logout(); > <span class= 'glyphicon glyphicon-off'></span> Log Out </div>");
+        loader.switchToShopper();
 
 
 
-}
-function toggleDriver(){
-    $("#index-Deliveries").remove();
-    $("#index-Tickets").remove();
-    $("#index-History").remove();
-    $("#index-LogOut").remove();
-
-    $("#Butt").append("<div class='panel-divs'id='index-Tickets' onclick=goToPage('_tickets'); > <span class= 'glyphicon glyphicon-list-alt'></span> Tickets </div>");
-    $("#Butt").append("<div class='panel-divs'id='index-Deliveries' onclick=goToPage('_yourDeliveries'); > <span class= 'glyphicon glyphicon-exclamation-sign'></span> Your Deliveries </div>");
-    $("#Butt").append("<div class='panel-divs'id='index-LogOut' onclick=loader.logout(); > <span class= 'glyphicon glyphicon-off'></span> Log Out </div>");
-    loader.switchToDriver();
-   
 
 
-
-}
-function toggles(){
-    if(toggle == "Driver"){
-        toggle = "Shopper";
-        document.getElementById('toggling').innerHTML = "Shopper"
-        toggleShopper();
     }
-    else{
-        toggle = "Driver";
-        document.getElementById('toggling').innerHTML = "Driver "
-        toggleDriver();
+    function toggleDriver(){
+        $("#index-Deliveries").remove();
+        $("#index-Tickets").remove();
+        $("#index-History").remove();
+        $("#index-LogOut").remove();
+
+        $("#Butt").append("<div class='panel-divs'id='index-Tickets' onclick=goToPage('_tickets'); > <span class= 'glyphicon glyphicon-list-alt'></span> Tickets </div>");
+        $("#Butt").append("<div class='panel-divs'id='index-Deliveries' onclick=goToPage('_yourDeliveries'); > <span class= 'glyphicon glyphicon-exclamation-sign'></span> Your Deliveries </div>");
+        $("#Butt").append("<div class='panel-divs'id='index-LogOut' onclick=loader.logout(); > <span class= 'glyphicon glyphicon-off'></span> Log Out </div>");
+        loader.switchToDriver();
+
+
+
+
     }
-}
+    function toggles(){
+        if(toggle == "Driver"){
+            toggle = "Shopper";
+            document.getElementById('toggling').innerHTML = "Shopper"
+            toggleShopper();
+        }
+        else{
+            toggle = "Driver";
+            document.getElementById('toggling').innerHTML = "Driver "
+            toggleDriver();
+        }
+    }
