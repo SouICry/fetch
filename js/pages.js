dabd57f85f00a0200aa1a84b83e030b757a4129f
Guyue Zhou <souicry.email@gmail.com>
2016-06-04 18:47:21 -0700

    Slowed down longer page transitions

--- js/pages.js
+++ js/pages.js
@@ -1,12 +1,22 @@
 var allPages = [
 
-    ["_homePage", "_shopping", "_checkout", "_deliveryTime", "_deliveryLocation", "_pendingPayment", "_submitted", "_cancelled"],
-    ["_history", "_shoppingStatus", "_cancelTicket"],
-    ['_confirmTicket', "_rateDriver", '_congratsTicketClosed'],
+    ["_homePage", "_shopping", "_checkout", "_deliveryTime", "_deliveryLocation", "_pendingPayment"],
+    ["_history", "_shoppingStatus", "_cancelTicket", "_submitted", "_cancelled"],
+    ['_confirmTicket', "_rateDriver", '_congratsTicketClosed',"_contact" ],
 
-    ['_tickets', '_viewTicket'],
+    ['_tickets', '_viewTicket',"_login","_signUp"],
     ["_yourDeliveries", '_driverList', "_receiptPictureEnterPrice", "_congratsDriverFinishShopping"],
-    ["_purchasedTickets", '_rateUser', '_congrats'],
+    ["_purchasedTickets", '_rateUser', '_congrats', "_verifyEmail"],
 
-    ["_accSetting", "_contact", "_passwordRecovery", "_passwordReset", "_login", "_signUp", "_takeProfilePicture", "_verifyEmail"]
+    ["_accSetting", "_takeProfilePicture", "_passwordRecovery", "_passwordReset"]
 ];
+//
+// ["_homePage", "_shopping", "_checkout", "_deliveryTime", "_deliveryLocation", "_pendingPayment", "_submitted", "_cancelled"],
+//     ["_history", "_shoppingStatus", "_cancelTicket"],
+//     ['_confirmTicket', "_rateDriver", '_congratsTicketClosed'],
+//
+//     ['_tickets', '_viewTicket'],
+//     ["_yourDeliveries", '_driverList', "_receiptPictureEnterPrice", "_congratsDriverFinishShopping"],
+//     ["_purchasedTickets", '_rateUser', '_congrats'],
+//
+//     ["_accSetting", "_contact", "_passwordRecovery", "_passwordReset", "_login", "_signUp", "_takeProfilePicture", "_verifyEmail"]
